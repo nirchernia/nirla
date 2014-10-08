13f113c11d2c7716d68b7542bbc5609d22046797
Nir Chernia <nirchernia@gmail.com>
2014-10-07 23:06:32 -0700

    implemented base temp

--- nirla/apps/suggest/forms.py
+++ nirla/apps/suggest/forms.py
@@ -1,15 +1,17 @@
 from django.forms import ModelForm
 from nirla.apps.suggest.models import *
+from django import forms
 
 
 class suggestionForm(ModelForm):
-    class Meta:
-        model = Suggestion
+	
+	class Meta:
+		model = Suggestion
         fields = (
             'name',
             'link',
             'email',
-            'message',
+            'message',           
             
         )
         
