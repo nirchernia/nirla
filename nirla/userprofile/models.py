19f6651a18aedb3efc6e0261de23fa0a3943e875
Nir Chernia <nirchernia@gmail.com>
2014-10-09 16:27:27 -0700

    added UserProfile model, did not syncdb

--- nirla/userprofile/models.py
+++ nirla/userprofile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from 
 
 # Create your models here.
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	followers = ManyToManyField('self', symmetrical=False, blank= True, null=True) #double check null and blank
+	following = ManyToManyField('self', symmetrical=False, blank=True, null=True)
+	likes = ManyToManyField(Article, blank=True, null=True)
+	
+	def __unicode__(self):
+		return u'%s' % self.user.username
+	
+	class Meta:
+		verbose_name = "User Profile"
+		verbose_name_plural = "user Profiles"
