c9790755962c7ae9b131c057423978cfaf694956
Nir Chernia <nirchernia@gmail.com>
2014-10-09 16:02:05 -0700

    started user

---
+++ nirla/userprofile/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+#login, logout, signup
+
+class Login(View):
+	
+	def get(self, request, *args, **kwargs):
+		pass
+	
+	
+	def post(self, request, *args, **kwargs):
+		pass
+		
+class Logout(View):
+	
+	def get(self, request, *args, **kwargs):
+		pass
+	
+	
+	def post(self, request, *args, **kwargs):
+		pass
+
+class Signup(View):
+	
+	def get(self, request, *args, **kwargs):
+		pass
+	
+	
+	def post(self, request, *args, **kwargs):
+		pass
